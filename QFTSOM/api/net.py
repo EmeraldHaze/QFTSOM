5bd66e5b882c6f8e334f1e177c3b7e6edfe910d8
Glycan Zyzzy <wisepoison@gmail.com>
2011-07-29 17:24:19 -0400

    Main engine completed

---
+++ QFTSOM/api/net.py
@@ -0,0 +1,4 @@
+class node:
+    def __init__(links, *args):
+        self.links = links
+        self.do = args
--- QFTSOM/main.py
+++ QFTSOM/main.py
@@ -1,1 +1,42 @@
-from core import battle
+from core import battle, haggle
+
+from content import battle_mods, glob_acts, people, meta
+from nodemap import nodemap
+
+
+###Start node actions
+def do_say(s): print s
+
+
+###Main function
+def travel(nodemap):
+    nodename = nodemap.start
+    node = nodemap[nodename]
+    while 1:
+        for do in node.does:
+            cmd, args = do.split(" ", 1)
+            globals()[cmd](args)
+
+        if node.exit_: return node.exit_
+        
+        if node.net:
+            nodename = traval(node)
+            
+        else:
+            ops = len(node.links)
+            #Print all options
+            for i in range(ops):print "(",i,")",node.links[i]
+            
+            #Get valid option
+            op = input(node.q)
+            while ops>op=>0:
+                print "invalid answer"
+                op = input(node.q)
+
+            #Get node name
+            nodename = node.names[op]
+
+        node = nodemap[nodename]
+            
+travel(nodemap)
+    
