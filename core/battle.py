b627ccd85cfe62293dcb31ab51444ffca525f974
Glycan Zyzzy <wisepoison@gmail.com>
2011-07-30 11:53:38 -0400

    Battle engine built into main, battle engine started

--- core/battle.py
+++ core/battle.py
@@ -1,1 +1,7 @@
-from api import skill
+def start(people, exits, mods):
+    setup()
+    #Sets up the clock, sceduals eavrybody, and generates ability lists. Also makes players list
+    while 1:
+        ###Get actions of all players of this tick
+        actions = []
+        for player in clock.players(): actions.append(player.think(players))
--- main.py
+++ main.py
@@ -7,22 +7,33 @@ from content.nodemap import nodemap
 ###Start node actions
 def do_say(s): print s
 
-
+def do_battle(arg):
+    persons, exits, mods = arg.split(" | ")
+    persons = [getattr(people, person) for person in persons.split(", ")]
+    mods = [getattr(battle_mods, mod) for mod in mods.split(", ")]
+    exits = [getattr(battle_mods, exit) for exit in exit.split(", ")]
+    battle.start(persons, exits, mods)
+    
 ###Main function
 def travel(nodemap):
     nodename = nodemap.start
     node = nodemap[nodename]
+    #Starting node in a network.
+    
     while 1:
         for do in node.does:
             cmd, args = do.split(" ", 1)
             globals()["do_"+cmd](args)
-
+        #Do all of the things the node wants done
         
         if node.net:
             nodename = travel(node)
+            #If the node is actually a subnetwork, travel it.
+            #It will return where the user exits, which will be the current node
             
         else:
-            #Check for exit
+            #If this node is an exit, return where it wants to exit too.
+            #Unless this is the root network, in which case it's a message to be printed
             if node.exit_: return node.exit_
             
             ops = len(node.links)
@@ -38,8 +49,9 @@ def travel(nodemap):
 
             #Get node name
             nodename = node.names[op]
-
-        node = nodemap[nodename]
             
+        node = nodemap[nodename]
+        #Set the node
+        
 print travel(nodemap)
-   
+#Travel the root network
