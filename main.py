8d43a9d98dc982ed5030b16fbbbb45635b206d3d
Glycan Zyzzy <wisepoison@gmail.com>
2011-08-23 08:57:40 -0400

    main.py now has pylint score of more 7+, almost a 5-point increase!

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+"""Starts the game, and defines highlevel noderunning"""
 from core import battle, haggle
 
 from data import battle_mods, glob_acts, beings, exits
@@ -5,9 +6,12 @@ from data.nodemap import nodemap
 
 
 ###Start node actions
-def do_say(s): print s
+def do_say(string):
+    """do_ node executer print function"""
+    print string
 
 def do_battle(arg):
+    """Start a battle- arg should be player1, player2|exit1, exit2|mod1, mod2"""
     players, game_exits, mods = arg.split("|")
     players = [getattr(beings, player) for player in players.split(", ")]
     game_exits = [getattr(exits, exit) for exit in game_exits.split(", ")]
@@ -16,6 +20,7 @@ def do_battle(arg):
     
 ###Main function
 def travel(nodemap):
+    """Travles the nodemap. This is recursive and calls itself on any submaps"""
     nodename = nodemap.start
     node = nodemap[nodename]
     #Starting node in a network.
@@ -35,15 +40,17 @@ def travel(nodemap):
             #If this node is an exit, return where it wants to exit too.
             #Unless this is the root network,
             #in which case exit_ will be a printed messege
-            if node.exit_: return node.exit_
+            if node.exit_:
+                return node.exit_
             
             ops = len(node.links)
             #Print all options
-            for i in range(ops):print "("+str(i)+")",node.links[i]
+            for i in range(ops):
+                print "("+str(i)+")", node.links[i]
             
             #Get valid option#############################################IO
             op = input(node.q+"\n")
-            while not 0<=op<ops:
+            while not 0 <= op < ops:
                 print "invalid answer"
                 op = input(node.q+"\n")
                 ##########################################################IO
