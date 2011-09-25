fff5d480496f918db6f7ea1d487abe81bdc78a6d
Glycan Zyzzy <wisepoison@gmail.com>
2011-09-24 20:55:24 -0400

    Changed libs/__init__.py for Magcius

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,11 +1,13 @@
-from collections import defaultdict as dd
+from collections import defaultdict
 __all__ = ['simple']#"statuses", "mardek", "basic", "misc"]
 
-class Lib:
-    def __getattr__(self, item):
-        return self.items[item]
-    def __init__(self, items = {}):
-        self.items = items
+#class Lib:
+#    def __init__(self, items = None):
+#        if items == None:
+#            items = {}
+#        self.items = items
+#    def __getattr__(self, item):
+#        return self.items[item]
 
 def get_componants(modules):
     comps = []
@@ -18,15 +20,15 @@ def get_componants(modules):
                 comp = getattr(m, comp_name)
                 print(comp)
                 comp.func = comp_name.split("_")[0] #Ex thinker, rule
-                comp.sys = module #Ex mana, limbs
+                comp.group = module #Ex mana, limbs
     return comps
 
 def _build_libs(comps):
-    func = dd(lambda :Lib())
-    sys = dd(lambda :Lib())
+    func = defaultdict(lambda :{})
+    group = defaultdict(lambda :{})
     for comp in comps:
-        sys[comp.sys].items[comp.func] = comp
-        func[comp.func].items[comp.sys] = func
-    return dict(sys.items() + sys.items())
+        group[comp.group].items[comp.func] = comp
+        group[comp.func].items[comp.group] = func
+    return func, group
 
-locals().update(_build_libs(get_componants(__all__)))
+func, group = _build_libs(get_componants(__all__))
