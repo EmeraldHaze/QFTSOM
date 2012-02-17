3bb2bb76989ba23959d649f49cf5c3fefcaac3fd
Glycan Zyzzy <wisepoison@gmail.com>
2012-02-16 22:09:19 -0500

    Fixed some things I overwrote with my findreplacer

---
+++ main.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Starts the game.
+"""
+PRELOAD_IMPORTS = False
+
+import sys, struct, pdb
+import game
+from core import shared
+
+if PRELOAD_IMPORTS:
+    import core, lib
+
+
+class Wrap:
+    """
+    This class wraps a FD for buffer flushing and [eventually] prefixing
+    messages with there lengths
+    """
+    def __init__(self, fd):
+        self.fd = fd
+
+    def write(self, data):
+        #data = struct.pack('!H', len(data)).decode() + '\x00' + data
+        self.fd.write(data)
+        self.fd.flush()
+
+    def __getattr__(self, item):
+        return getattr(self.fd, item)
+
+
+sys.stdout = Wrap(sys.stdout)
+
+print("This is QFTSOM 0.2. In lieu of a proper interface, use the numbers. "
+      " Failure to do so can cause crashs.")
+
+shared.name = input()
+
+
+if __name__ == "__main__":
+    print(game.nodemap.travel())
